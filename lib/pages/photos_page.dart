cc1cb99bec99bb1954f7580d20a942f32682cf54
jcblanc2 <jclaytonblanc@gmail.com>
2023-08-31 11:16:47 -0400

    Add Staggered Gridview

--- lib/pages/photos_page.dart
+++ lib/pages/photos_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 
 class PhotosPage extends StatefulWidget {
   const PhotosPage({super.key});
@@ -57,6 +58,18 @@ class _PhotosPageState extends State<PhotosPage> {
                         ))
                     .toList(),
               )),
+          Expanded(
+            child: MasonryGridView.builder(
+                itemCount: 4,
+                gridDelegate:
+                    const SliverSimpleGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 3),
+                itemBuilder: ((context, index) => Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: ClipRRect(
+                        borderRadius: BorderRadius.circular(12),
+                        child: Image.asset('lib/images/imd.jpg'))))),
+          )
         ],
       ),
     );
