1bdc1cfef664f5b7fd59c7a29b7d6f3b9a889c65
John Clayton Blanc <108766949+jcblanc2@users.noreply.github.com>
2023-09-04 15:10:49 -0400

    Update constants.dart

--- lib/services/constants.dart
+++ lib/services/constants.dart
@@ -1,8 +1,6 @@
 class Constants {
   static String baseUrl = 'https://api.unsplash.com';
   static String photosEndpoint = '/search/photos';
-  static String apiKey = "2iNsuO1BhimfAYwjdXh7xdU88Cje4H3pGz3h_rsm4UA";
-  static String secret = "aUpWijZunetwJHtYTwoSWoW_Xi3f8W0cXaEfK8CQyaw";
   static const String initialRoute = '/';
   static const String detailRoute = '/details';
 }
